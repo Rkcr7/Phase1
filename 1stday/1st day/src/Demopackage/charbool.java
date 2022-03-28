33a122d48db0b1531946a47c97e41282cc26412c
Ritik Sharma <ritiksharma@BLR.ALLEGISINDIA.COM>
2022-03-28 22:39:16 +0530

    DAY 1 codes

---
+++ 1stday/1st day/src/Demopackage/charbool.java
@@ -0,0 +1,23 @@
+package Demopackage;
+
+public class charbool {
+	public static void main(String[] args) {
+		
+		char mychar='R';
+		System.out.println(mychar);
+		
+		char myuni='\u00A9';
+		System.out.println(myuni);
+		
+		//(boolean)
+		boolean x = true;
+		boolean y = false;
+		if (x==true) {
+			System.out.println("XO");
+		}
+
+		
+		
+	}
+
+}
